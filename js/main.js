e611e36f6cc417959432b1765cabdfd135315a74
kaylindenberg <kay.lindneberg@gmail.com>
2015-05-06 15:16:47 -0500

    fixed slider

--- js/main.js
+++ js/main.js
@@ -72,15 +72,15 @@
 		
 		//Home Background slider
 		jQuery.supersized({	
-		slide_interval          :   1000,		// Length between transitions
+		slide_interval          :   3000,		// Length between transitions
 		transition              :   1, 			// 0-None, 1-Fade, 2-Slide Top, 3-Slide Right, 4-Slide Bottom, 5-Slide Left, 6-Carousel Right, 7-Carousel Left
-		transition_speed		:	500,		// Speed of transition				
+		transition_speed		:	700,		// Speed of transition				
 		slide_links				:	'blank',	// Individual links for each slide (Options: false, 'num', 'name', 'blank')
 		slides 					:  	[			// Slideshow Images
-										{image : 'img/NUHomecoming.jpg'},
-										{image : 'img/Marathon2015.jpg'},  
+										{image : 'img/hands.jpg'},
+										{image : 'img/NUHomecoming.jpg'},  
+										{image : 'img/Birthday2014.jpg'},
 										{image : 'img/Bulls2014.jpg'}
-										{image : 'img/Birthday2014.jpg'}
 									]
 		});	
 	
